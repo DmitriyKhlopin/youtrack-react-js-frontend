bc25f658550133c3df631fa903063346e02b7aac
dmitriy.khlopin <qwerten85@gmail.com>
2019-02-20 13:01:16 +0500

    Added DurationDisplay

--- src/components/DurationDisplay.js
+++ src/components/DurationDisplay.js
@@ -1,22 +1,18 @@
 import React, {Component} from "react";
-import moment from "moment";
-import ReactTable from "react-table";
 import store from "../redux/store";
 import {setSelectedNavItem} from "../redux/actions/appBarActions";
 import * as PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import {styles} from "../Styles";
 import connect from "react-redux/es/connect/connect";
-import FilterIcon from '@material-ui/icons/Settings';
 import Button from "@material-ui/core/Button/Button";
-import TimeAccountingFilterDialog from "./TimeAccountingFilterDialog";
 import {fetchTimeAccountingData} from "../redux/actions/timeAccountingActions";
-import RefreshIcon from '@material-ui/icons/Refresh';
 import {PAGE_IDS} from "../Const";
+import FormControl from "./FixedDefectsDisplay";
 
 //TODO style={classes.content} causes crashes in firefox
 
-class TimeAccountingDisplay extends Component {
+class DurationDisplay extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -30,116 +26,62 @@ class TimeAccountingDisplay extends Component {
         this.isCancelled = true;
     }
 
-    componentWillMount() {
-        store.dispatch(setSelectedNavItem({title: PAGE_IDS.duration.name, selectedId: PAGE_IDS.duration.id}));
-    }
-
     requestData = () => {
         store.dispatch(fetchTimeAccountingData());
     };
 
-    handleClickOpen = scroll => () => {
-        this.setState({open: true, scroll});
-    };
-
-    handleClose = () => {
-        this.requestData();
-        this.setState({open: false});
-    };
-
     componentDidMount() {
-        store.dispatch(fetchTimeAccountingData());
+        console.log('CDM');
+        store.dispatch(setSelectedNavItem({title: PAGE_IDS.duration.name, selectedId: PAGE_IDS.duration.id}));
+        /*store.dispatch(fetchIterations());*/
+        /*this.setState({
+            labelWidth2: ReactDOM.findDOMNode(this.InputLabelRef2).offsetWidth,
+        });*/
     }
+
+    handleChange = event => {
+        /*store.dispatch(clearWorkItems());*/
+        this.setState({[event.target.name]: event.target.value});
+        /*if (event.target.name === 'iteration') store.dispatch(fetchBuildsByIteration(this.props.fixedDefectsData.iterations[event.target.value]));*/
+        console.log(event.target.value);
+    };
 
     render() {
         const {classes} = this.props;
-        const items = this.props.timeAccountingData.timeData;
-        /*if (items === null) return <div>Loading</div>;
-        if (items.length === 0) return <div>No items to display</div>;*/
-        /*const i = items.map(item => item.id).reduce(
-            (a, b, i, arr) =>
-                (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-            null);*/
-        const columns = [{
-            Header: 'ID',
-            accessor: 'id',
-            Footer: (
-                <span><strong>Popular: </strong>{" "} {items.map(item => item.id).reduce(
-                    (a, b, i, arr) =>
-                        (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-                    null)}</span>
-            ),
-            filterMethod: (filter, row) =>
-                row[filter.id].startsWith(filter.value) ||
-                row[filter.id].endsWith(filter.value)
-        },
-            {
-                Header: 'User', accessor: 'agent', filterMethod: (filter, row) =>
-                    row[filter.id].startsWith(filter.value) ||
-                    row[filter.id].endsWith(filter.value)
-            },
-            {Header: 'Трудозатраты', accessor: 'units',},
-            {id: 'crDate', Header: 'Дата', accessor: d => moment(d.changedDate).format('YYYY-MM-DD')},
-            {
-                Header: 'Проект',
-                accessor: 'projects',
-                Footer: (
-                    <span><strong>Popular: </strong>{" "} {items.map(item => item.projects).reduce(
-                        (a, b, i, arr) =>
-                            (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-                        null)}</span>
-                )
-            },
-            {
-                Header: 'Этап',
-                accessor: 'iterationPath',
-                Footer: (
-                    <span><strong>Popular: </strong>{" "} {items.map(item => item.iterationPath).reduce(
-                        (a, b, i, arr) =>
-                            (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-                        null)}</span>
-                ),
-                filterMethod: (filter, row) =>
-                    row[filter.id].startsWith(filter.value) ||
-                    row[filter.id].endsWith(filter.value)
-            }];
-
+        /*const items = this.props.timeAccountingData.timeData;*/
+        console.log(this.props.reportFilters);
         return <div style={{minWidth: '100%'}}>
-            <ReactTable
+            <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+            }}>
 
-                /*style={classes.content}*/
-                data={items}
-                filterable
-                defaultFilterMethod={(filter, row) =>
-                    String(row[filter.id]) === filter.value}
-                columns={columns}
-            />
-            <TimeAccountingFilterDialog open={this.state.open}
-                                        handleClose={this.handleClose}
-                                        aria-labelledby="scroll-dialog-title"/>
-            <Button variant="fab" mini className={classes.fabLoad} color={'secondary'}
-                    onClick={this.requestData}>
-                <RefreshIcon/>
-            </Button>
-            <Button variant="fab" mini className={classes.fab} color={'primary'}
-                    onClick={this.handleClickOpen('paper')}>
-                <FilterIcon/>
-            </Button>
-        </div>
+
+                <FormControl variant="outlined" className={classes.formControl}>
+                    <Button variant="contained" color="primary" className={classes.button2}
+                            onClick={() => {
+                                /*store.dispatch(getFixedByIterationAndBuild(this.props.fixedDefectsData.iterations[this.state.iteration], this.state.build));
+                                store.dispatch(clearWorkItems());*/
+                            }
+                            }>
+                        Загрузить
+                    </Button>
+                </FormControl>
+            </div>
+        </div>;
     }
 }
 
-TimeAccountingDisplay.propTypes = {
+DurationDisplay.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
     return {
-        /*reportFilters: state.reportFilters,
-        reports: state.reports,*/
         appBarState: state.appBarState,
         timeAccountingData: state.timeAccountingData,
+        reportFilters: state.reportFilters
     }
 }
 
-export default withStyles(styles, {withTheme: true})(connect(mapStateToProps, null)(TimeAccountingDisplay));
+export default withStyles(styles, {withTheme: true})(connect(mapStateToProps, null)(DurationDisplay));
