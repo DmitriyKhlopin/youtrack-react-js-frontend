c2ef4271f1e49aed4e4efba4b354f40c41050768
dmitriy.khlopin <qwerten85@gmail.com>
2019-08-05 13:45:36 +0500

    Added linear progressbar.

--- src/components/TimeAccountingDisplay.js
+++ src/components/TimeAccountingDisplay.js
@@ -1,25 +1,27 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import moment from "moment";
 import ReactTable from "react-table";
 import store from "../redux/store";
 import {setSelectedNavItem} from "../redux/actions/appBarActions";
-import * as PropTypes from "prop-types";
 import connect from "react-redux/es/connect/connect";
 import {fetchTimeAccountingData} from "../redux/actions/timeAccountingActions";
 import {PAGES} from "../Const";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
-class TimeAccountingDisplay extends Component {
-    componentWillMount() {
-        store.dispatch(setSelectedNavItem(PAGES.filter((page) => page.path === this.props.location.pathname)[0]));
-    }
-
-    componentDidMount() {
+function TimeAccountingDisplay({location, timeAccountingData}) {
+    useEffect(() => {
+        store.dispatch(setSelectedNavItem(PAGES.filter((page) => page.path === location.pathname)[0]));
         store.dispatch(fetchTimeAccountingData());
-    }
+    }, []);
 
-    render() {
-        const items = this.props.timeAccountingData.timeData;
-        const columns = [{
+    const items = timeAccountingData.timeData;
+    let table;
+    let columns;
+    if (items === null) table = <div>Error</div>;
+    if (items && timeAccountingData.timeLoading === true) table = <LinearProgress style={{marginTop: 8}}/>;
+    if (items && timeAccountingData.timeLoading === false && items.length === 0) table = <div>No items to display</div>;
+    if (items && items.length !== 0) columns = [
+        {
             Header: 'ID задачи',
             accessor: 'id',
             Footer: (
@@ -32,63 +34,62 @@ class TimeAccountingDisplay extends Component {
                 row[filter.id].startsWith(filter.value) ||
                 row[filter.id].endsWith(filter.value)
         },
-            {
-                Header: 'Сотрудник', accessor: 'agent', filterMethod: (filter, row) =>
-                    row[filter.id].startsWith(filter.value) ||
-                    row[filter.id].endsWith(filter.value)
-            },
-            {Header: 'Трудозатраты', accessor: 'units',},
-            {id: 'crDate', Header: 'Дата', accessor: d => moment(d.changedDate).format('YYYY-MM-DD')},
-            {
-                Header: 'Проект',
-                accessor: 'projects',
-                Footer: (
-                    <span><strong>Popular: </strong>{" "} {items.map(item => item.projects).reduce(
-                        (a, b, i, arr) =>
-                            (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-                        null)}</span>
-                )
-            },
-            {
-                Header: 'Этап',
-                accessor: 'iterationPath',
-                Footer: (
-                    <span><strong>Popular: </strong>{" "} {items.map(item => item.iterationPath).reduce(
-                        (a, b, i, arr) =>
-                            (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-                        null)}</span>
-                ),
-                filterMethod: (filter, row) =>
-                    row[filter.id].startsWith(filter.value) ||
-                    row[filter.id].endsWith(filter.value)
-            },
-            {
-                Header: 'Приоритет',
-                accessor: 'priority',
-                Footer: (
-                    <span><strong>Popular: </strong>{" "} {items.map(item => item.priority).reduce(
-                        (a, b, i, arr) =>
-                            (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
-                        null)}</span>
-                ),
-                filterMethod: (filter, row) =>
-                    row[filter.id].startsWith(filter.value) ||
-                    row[filter.id].endsWith(filter.value)
-            }];
+        {
+            Header: 'Сотрудник', accessor: 'agent', filterMethod: (filter, row) =>
+                row[filter.id].startsWith(filter.value) ||
+                row[filter.id].endsWith(filter.value)
+        },
+        {Header: 'Трудозатраты', accessor: 'units',},
+        {id: 'crDate', Header: 'Дата', accessor: d => moment(d.changedDate).format('YYYY-MM-DD')},
+        {
+            Header: 'Проект',
+            accessor: 'projects',
+            Footer: (
+                <span><strong>Popular: </strong>{" "} {items.map(item => item.projects).reduce(
+                    (a, b, i, arr) =>
+                        (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
+                    null)}</span>
+            )
+        },
+        {
+            Header: 'Этап',
+            accessor: 'iterationPath',
+            Footer: (
+                <span><strong>Popular: </strong>{" "} {items.map(item => item.iterationPath).reduce(
+                    (a, b, i, arr) =>
+                        (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
+                    null)}</span>
+            ),
+            filterMethod: (filter, row) =>
+                row[filter.id].startsWith(filter.value) ||
+                row[filter.id].endsWith(filter.value)
+        },
+        {
+            Header: 'Приоритет',
+            accessor: 'priority',
+            Footer: (
+                <span><strong>Popular: </strong>{" "} {items.map(item => item.priority).reduce(
+                    (a, b, i, arr) =>
+                        (arr.filter(v => v === a).length >= arr.filter(v => v === b).length ? a : b),
+                    null)}</span>
+            ),
+            filterMethod: (filter, row) =>
+                row[filter.id].startsWith(filter.value) ||
+                row[filter.id].endsWith(filter.value)
+        }
+    ];
 
-        return (<div style={{width: '100%'}}>
-            <ReactTable data={items}
-                        filterable
-                        defaultFilterMethod={(filter, row) =>
-                            String(row[filter.id]) === filter.value}
-                        columns={columns}
-            /></div>)
+    if (columns) {
+        table = <ReactTable
+            data={items}
+            filterable
+            defaultFilterMethod={(filter, row) =>
+                String(row[filter.id]) === filter.value}
+            columns={columns}
+        />
     }
+    return (<div style={{width: '100%'}}>{table}</div>)
 }
-
-TimeAccountingDisplay.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
 function mapStateToProps(state) {
     return {
--- src/redux/reducers/timeAccountingReducers.js
+++ src/redux/reducers/timeAccountingReducers.js
@@ -1,19 +1,29 @@
 export default function reducer(state = {
+    timeLoading: false,
     timeData: [],
     dictionaryData: []
 }, action) {
     switch (action.type) {
         case 'FETCH_TIME_ACCOUNTING_PENDING': {
+            state = {
+                ...state,
+                timeLoading: true
+            };
             break;
         }
         case 'FETCH_TIME_ACCOUNTING_FULFILLED': {
             state = {
                 ...state,
+                timeLoading: false,
                 timeData: action.payload
             };
             break;
         }
         case 'FETCH_TIME_ACCOUNTING_FAILED': {
+            state = {
+                ...state,
+                timeLoading: false,
+            };
             break;
         }
         case 'FETCH_TIME_ACCOUNTING_DICTIONARY_PENDING': {
